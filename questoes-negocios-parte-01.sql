a4cca53e15a5d2469310f5144de4c8c0368b2ba0
Edinan Marinho <61295729+EdinanMarinho@users.noreply.github.com>
2025-03-14 14:48:14 -0300

    respondendo-questoes-negocios-parte01

---
+++ questoes-negocios-parte-01.sql
@@ -0,0 +1,40 @@
+-- Respondendo Questões de Negócios (Nível Básico (Exploração Geral))
+
+-- 1. Quantas vendas e devoluções foram registradas no total?
+
+
+SELECT a.Status
+	 , SUM(a.Qtde) AS Qtde
+  FROM
+(
+SELECT * FROM Vendas
+UNION
+SELECT * FROM Devolucoes 
+) a
+ GROUP
+	BY a.Status
+ ORDER 
+	BY Qtde DESC
+
+-- Usando Tabela Temporaria
+
+SELECT Status
+	 , SUM(Qtde) AS Qtde
+  FROM #tbl_operacao
+ GROUP
+	BY Status
+  
+-- Criando Tabela Temporaria
+SELECT 
+* 
+  INTO #tbl_operacao
+  FROM
+(
+SELECT * FROM Vendas
+UNION
+SELECT * FROM Devolucoes 
+) a
+
+-- 2. Qual é o valor total das vendas realizadas?
+-- 3. Quantos clientes distintos fizeram compras?
+
